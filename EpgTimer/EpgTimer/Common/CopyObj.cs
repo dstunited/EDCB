58ceac62d83b5085841baa458b7ddad05fda289d
tkntrec <Tokinata@gmail.com>
2015-03-15 20:30:51 +0900

    CopyObj.csを修正
    820a87f3c6a41f4ac6fcd27f33007b360340e48c の続き
    今さらだけど修正
    動作への影響は特になし

--- EpgTimer/EpgTimer/Common/CopyObj.cs
+++ EpgTimer/EpgTimer/Common/CopyObj.cs
@@ -12,37 +12,30 @@ namespace EpgTimer
     {
         //シリアライズとかもろもろは使えないので地道にコピーする。
         //ジェネリックは手続きの統一用で、各メソッドは別途定義する。
-        private static List<T> Clone<T>(this List<T> src) where T : class, new()
+
+        private static List<T> Clone<T>(this List<T> src, Action<T, T> CopyData) where T : class, new()
         {
             if (src == null) return null; //普通にnullで呼び出せてしまう
             List<T> copylist = new List<T>();
-            foreach (T item in src) copylist.Add(item.Clone());
+            foreach (T item in src) copylist.Add(item.Clone(CopyData));
             return copylist;
         }
-        private static T Clone<T>(this T src) where T : class, new()
+        private static T Clone<T>(this T src, Action<T, T> CopyData) where T : class, new()
         {
             if (src == null) return null; //普通にnullで呼び出せてしまう
             T copyobj = new T();
-            src.CopyTo(copyobj);
+            src.CopyTo(copyobj, CopyData);
             return copyobj;
         }
-        private static void CopyTo<T>(this T src, T dest)
+        private static void CopyTo<T>(this T src, T dest, Action<T, T> CopyData)
         {
             if (src == null || dest == null) return;
-
-            //もっといいやり方があるはず‥それともこのやり方諦めた方がいいのか？
-            switch (src.GetType().Name)
-            {
-                case "EpgSearchKeyInfo": CopyData(src as EpgSearchKeyInfo, dest as EpgSearchKeyInfo); break;
-                case "EpgContentData": CopyData(src as EpgContentData, dest as EpgContentData); break;
-                case "EpgSearchDateInfo": CopyData(src as EpgSearchDateInfo, dest as EpgSearchDateInfo); break;
-                default: break;
-            }
+            CopyData(src, dest);
         }
 
-        public static List<EpgSearchKeyInfo> Clone(this List<EpgSearchKeyInfo> src) { return src.Clone<EpgSearchKeyInfo>(); }
-        public static EpgSearchKeyInfo Clone(this EpgSearchKeyInfo src) { return src.Clone<EpgSearchKeyInfo>(); }
-        public static void CopyTo(this EpgSearchKeyInfo src, EpgSearchKeyInfo dest) { src.CopyTo<EpgSearchKeyInfo>(dest); }
+        public static List<EpgSearchKeyInfo> Clone(this List<EpgSearchKeyInfo> src) { return src.Clone<EpgSearchKeyInfo>(CopyData); }
+        public static EpgSearchKeyInfo Clone(this EpgSearchKeyInfo src) { return src.Clone<EpgSearchKeyInfo>(CopyData); }
+        public static void CopyTo(this EpgSearchKeyInfo src, EpgSearchKeyInfo dest) { src.CopyTo<EpgSearchKeyInfo>(dest, CopyData); }
         private static void CopyData(EpgSearchKeyInfo src, EpgSearchKeyInfo dest)
         {
             dest.aimaiFlag = src.aimaiFlag;
@@ -65,9 +58,9 @@ namespace EpgTimer
             dest.chkDurationMax = src.chkDurationMax;
         }
 
-        public static List<EpgContentData> Clone(this List<EpgContentData> src) { return src.Clone<EpgContentData>(); }
-        public static EpgContentData Clone(this EpgContentData src) { return src.Clone<EpgContentData>(); }
-        public static void CopyTo(this EpgContentData src, EpgContentData dest) { src.CopyTo<EpgContentData>(dest); }
+        public static List<EpgContentData> Clone(this List<EpgContentData> src) { return src.Clone<EpgContentData>(CopyData); }
+        public static EpgContentData Clone(this EpgContentData src) { return src.Clone<EpgContentData>(CopyData); }
+        public static void CopyTo(this EpgContentData src, EpgContentData dest) { src.CopyTo<EpgContentData>(dest, CopyData); }
         private static void CopyData(EpgContentData src, EpgContentData dest)
         {
             dest.content_nibble_level_1 = src.content_nibble_level_1;
@@ -76,9 +69,9 @@ namespace EpgTimer
             dest.user_nibble_2 = src.user_nibble_2;
         }
 
-        public static List<EpgSearchDateInfo> Clone(this List<EpgSearchDateInfo> src) { return src.Clone<EpgSearchDateInfo>(); }
-        public static EpgSearchDateInfo Clone(this EpgSearchDateInfo src) { return src.Clone<EpgSearchDateInfo>(); }
-        public static void CopyTo(this EpgSearchDateInfo src, EpgSearchDateInfo dest) { src.CopyTo<EpgSearchDateInfo>(dest); }
+        public static List<EpgSearchDateInfo> Clone(this List<EpgSearchDateInfo> src) { return src.Clone<EpgSearchDateInfo>(CopyData); }
+        public static EpgSearchDateInfo Clone(this EpgSearchDateInfo src) { return src.Clone<EpgSearchDateInfo>(CopyData); }
+        public static void CopyTo(this EpgSearchDateInfo src, EpgSearchDateInfo dest) { src.CopyTo<EpgSearchDateInfo>(dest, CopyData); }
         private static void CopyData(EpgSearchDateInfo src, EpgSearchDateInfo dest)
         {
             dest.endDayOfWeek = src.endDayOfWeek;
